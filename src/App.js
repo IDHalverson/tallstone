a3d4eec46ec5472930996037cb8ed927f1647ca2
Ian Halverson <ihalverson@burrislogistics.com>
2021-10-20 08:22:29 -0400

    initial commit - create react app - add tallstone temporary logos

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import tallstone from './assets/images/tallstone.jpg';
+import './App.css';
+
+const App = () => (
+  <div className="App">
+    <header className="App-header">
+      <img src={tallstone} alt="logo" />
+    </header>
+  </div>
+)
+
+export default App;
